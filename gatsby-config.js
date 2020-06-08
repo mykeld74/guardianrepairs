6d037e22be85344ca7a3a27d80c4c7267baf0322
Mike Daugherty <mikedaugherty@msdwebdesigns.com>
2020-06-08 13:02:33 -0600

    Remove css purge

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: "Guardian Garage Doors",
-    description: "Garage Door Replacement and Repair",
-    author: "Mike Daugherty",
+    title: 'Guardian Garage Doors',
+    description: 'Garage Door Replacement and Repair',
+    author: 'Mike Daugherty',
   },
   plugins: [
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sass",
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -20,14 +20,14 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-source-wordpress",
+      resolve: 'gatsby-source-wordpress',
       options: {
         // The base url to your WP site.
-        baseUrl: "backend.guardianrepairs.com",
+        baseUrl: 'backend.guardianrepairs.com',
         // WP.com sites set to true, WP.org set to false
         hostingWPCOM: false,
         // The protocol. This can be http or https.
-        protocol: "https",
+        protocol: 'https',
         // Use 'Advanced Custom Fields' Wordpress plugin
         useACF: true,
         auth: {},
@@ -35,18 +35,18 @@ module.exports = {
         verboseOutput: false,
       },
     },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      // Removes unused css rules
-      resolve: "gatsby-plugin-purgecss",
-      options: {
-        // Activates purging in gatsby develop
-        develop: true,
-        // Purge only the main css file
-        purgeOnly: ["/all.sass"],
-      },
-    }, // must be after other CSS plugins
-    "gatsby-plugin-netlify", // make sure to keep it last in the array
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    // {
+    //   // Removes unused css rules
+    //   resolve: "gatsby-plugin-purgecss",
+    //   options: {
+    //     // Activates purging in gatsby develop
+    //     develop: true,
+    //     // Purge only the main css file
+    //     purgeOnly: ["/all.sass"],
+    //   },
+    // }, // must be after other CSS plugins
+    'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
